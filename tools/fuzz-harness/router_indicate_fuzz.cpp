8f4dcfb999d0f55b850c6e391e4bcb27fd72f62b
Leonhard Bosch <leonhard.bosch@icloud.com>
2024-05-16 14:53:08 +0200

    Finalized PAPI benchmarking

--- tools/fuzz-harness/router_indicate_fuzz.cpp
+++ tools/fuzz-harness/router_indicate_fuzz.cpp
@@ -1,6 +1,17 @@
+#define ABCD
+
 #include <stdio.h>
 #include <unistd.h>
 #include "RouterIndicate.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#ifdef ABCD
+#include <papi.h>
+#include "Base64.h"
+#endif
+
 
 
 #ifndef __AFL_FUZZ_TESTCASE_LEN
@@ -15,13 +26,54 @@ unsigned char fuzz_buf[1024000];
 
 __AFL_FUZZ_INIT();
 
+
+#ifdef ABCD
+void handle_error (int retval)
+{
+    printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
+    exit(1);
+}
+
+void setupPAPI(int &retval, int &eventSet) {
+    /* Initialize the PAPI library */
+    retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT)
+        handle_error(retval);
+
+    /* Create the Event Set */
+    retval = PAPI_create_eventset(&eventSet);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+
+    /* Add Total Instructions Executed to our Event Set */
+    retval = PAPI_add_event(eventSet, PAPI_TOT_INS);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+
+    /* Start counting events in the Event Set */
+    retval = PAPI_start(eventSet);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+}
+#endif
+
 int main() {
 #ifdef __AFL_HAVE_MANUAL_CONTROL
     __AFL_INIT();
 #endif
 
+#ifdef ABCD
+    int retval;
+    int eventSet = PAPI_NULL;
+    long_long values[1];
+
+    setupPAPI(retval, eventSet);
+#endif
     RouterIndicate routerIndicate;
     routerIndicate.SetUp();
+
+    std::ofstream outfile;
+    outfile.open("fuzzStat.csv", std::ios_base::app);
 
     unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
 
@@ -30,10 +82,30 @@ int main() {
 
         ByteBuffer buffer = ByteBuffer(buf, buf + len);
 
+#ifdef ABCD
+        retval = PAPI_reset(eventSet);
+        if (retval != PAPI_OK)
+            handle_error(retval);
+#endif
+
         routerIndicate.router.indicate(routerIndicate.get_up_packet(buffer),
                                        routerIndicate.mac_address_sender,
                                        routerIndicate.mac_address_destination);
-    }
+#ifdef ABCD
+        retval = PAPI_read(eventSet, values);
+        if (retval != PAPI_OK)
+            handle_error(retval);
 
+        if(values[0]>39700000) {
+            outfile << "\n" << values[0] << ", " <<  len << ", " << base64::encode(buffer);
+        }
+#endif
+    }
+#ifdef ABCD
+    /* Stop the counting of events in the Event Set */
+    retval = PAPI_stop(eventSet, values);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+#endif
     return 0;
 }
--- tools/fuzz-harness/router_indicate_test.cpp
+++ tools/fuzz-harness/router_indicate_test.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <vector>
 #include <papi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void handle_error (int retval)
 {
@@ -30,6 +32,27 @@ ByteBuffer readFileToByteArray(const std::string &filename) {
     return buffer;
 }
 
+void setupPAPI(int &retval, int &eventSet) {
+    /* Initialize the PAPI library */
+    retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT)
+        handle_error(retval);
+
+    /* Create the Event Set */
+    retval = PAPI_create_eventset(&eventSet);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+
+    /* Add Total Instructions Executed to our Event Set */
+    retval = PAPI_add_event(eventSet, PAPI_TOT_INS);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+
+    /* Start counting events in the Event Set */
+    retval = PAPI_start(eventSet);
+    if (retval != PAPI_OK)
+        handle_error(retval);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -44,19 +67,37 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int retval;
+    int eventSet = PAPI_NULL;
+    long_long values[1];
+
+    setupPAPI(retval, eventSet);
+
     RouterIndicate routerIndicate;
     routerIndicate.SetUp();
 
-    int retval = PAPI_hl_region_begin("computation");
-    if ( retval != PAPI_OK )
-        handle_error(retval);
+    std::ofstream outfile;
+    outfile.open("test.txt", std::ios_base::app);
 
-    routerIndicate.router.indicate(routerIndicate.get_up_packet(byteArray),
+    for(int i = 0; i < 100; i++) {
+        retval = PAPI_reset(eventSet);
+        if (retval != PAPI_OK)
+            handle_error(retval);
+
+        routerIndicate.router.indicate(routerIndicate.get_up_packet(byteArray),
                                    routerIndicate.mac_address_sender,
                                    routerIndicate.mac_address_destination);
 
-    retval = PAPI_hl_region_end("computation");
-    if ( retval != PAPI_OK )
+        retval = PAPI_read(eventSet, values);
+        if (retval != PAPI_OK)
+            handle_error(retval);
+
+        outfile << "\n" << values[0] << ", " <<  byteArray.size();
+    }
+
+    /* Stop the counting of events in the Event Set */
+    retval = PAPI_stop(eventSet, values);
+    if (retval != PAPI_OK)
         handle_error(retval);
 
     return 0;
